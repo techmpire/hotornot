a057cc9d79c992f9704a961d63a47cb182025ac2
Aleks Sotirovski <aleks.s@mpiremedia.com.au>
2014-07-31 17:36:32 +0800

    styling

--- index.php
+++ index.php
@@ -28,15 +28,13 @@
     $app->view(new \Slim\Views\Twig());
     $app->view->parserOptions = array(
         'charset'             => 'utf-8',
-        'cache'               => false, //'templates/cache',
+        'cache'               => false, 
         'auto_reload'         => true,
         'strict_variables'    => false,
         'autoescape'          => true
     );
-    //$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+    $app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
     
-    // Routes:
-    // Home
     $app->get('/', function () use ($app) {
         $app->render('home.html');
     });
@@ -46,35 +44,58 @@
     });
     
     $app->post('/home', function () use ($app) {
+        
+        // validate...
+        
         $post = $app->request->post();
         $user = new User();
         $user->first_name = $post['first_name'];
         $user->last_name = $post['last_name'];
         $user->email = $post['email'];
+        $user->phone = $post['phone'];
         $user->save();
         $_SESSION['digitalx_hash'] = $user->hash;
         $app->response->redirect('/refer');
     });
     
-    // Refer
     $app->get('/refer', function () use ($app) {
         $app->render('refer.html');
     });
     
     $app->post('/refer', function () use ($app) {
     
-        $friends = explode(',', $app->request->post('friends'));
+        if (empty($_SESSION['digitalx_hash'])) {
+            $app->response->redirect('/home');
+            exit;
+        }
+        
+        if (sizeof($app->request->post('names')) == 0 || sizeof($app->request->post('emails')) == 0) {
+            $app->response->redirect('/refer');
+            exit;
+        }
+        
         $user = User::find_by_hash($_SESSION['digitalx_hash']);
         
         if (!is_numeric($user->id)) {
             exit('user hash not found');
         } else {
-            foreach ($friends as $friend) {
-                $friend = new Friend(array(
-                    'user_id' => $user->id,
-                    'friend_email' => trim($friend)
-                ));
-                $friend->save();
+        
+            foreach ($app->request->post('names') as $key => $name) {
+                
+                $email = $app->request->post('emails')[$key];
+                
+                if ($name != '' and $email != '') {
+                
+                    $friend = new Friend(array(
+                        'user_id' => $user->id,
+                        'name' => trim($name),
+                        'email' => trim($email)
+                    ));
+                    
+                    $friend->save();
+                
+                }
+                
             }
             
         }
