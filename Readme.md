2cc3f0768ffd100a326493786d34d0a5b5b35040
Aleks Sotirovski <aleks.s@mpiremedia.com.au>
2014-10-23 15:58:05 +0800

    updated readme

---
+++ Readme.md
@@ -0,0 +1,4 @@
+### Setup notes
+- The test assumes you have setup http://devtest.local as your local dev host
+- Import db.sql
+- Update config.php to match your local db credentials
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit69d862f8e5f6a154489c8b3c58a0e9ec::getLoader();
+return ComposerAutoloaderInitf61ac14f7ea35a95da8931f6f35614e0::getLoader();
